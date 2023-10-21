c73efdb2cbff77148dc99f3dbfc443d629aeb6f9
Ashesh <ashesh276@gmail.com>
2023-10-21 22:48:13 +0200

    added contrastive learning results

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Bayesian Methods For Machine Learning",
         "excerpt":"This post will be a summary of my understanding from the coursera course Bayesian methods for Machine learning. Let me first honestly admit that this page was not created by me to either educate the uneducated or to make myself believe that the educated ones will recognize me as an...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Bayesian-methods-for-machine-learning/",
+        "url": "http://localhost:4001/Bayesian-methods-for-machine-learning/",
         "teaser": null
       }]
